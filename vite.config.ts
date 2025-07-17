c06d37fee82864ddc7794cf0e44b6ee380e84f67
YashB28 <yash.ec21@bmsce.ac.in>
2025-07-18 01:41:30 +0530

    path changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'OG_Website',  // ← important!
+  base: '/OG_Website/',  // ← important!
   plugins: [react()],
 });
